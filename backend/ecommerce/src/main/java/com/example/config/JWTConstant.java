f4849942120d59187ed5846ea96142c54ae28e35
Dollsy Rani <dollsyrani155@gmail.com>
2025-09-08 01:29:28 +0530

    backend

---
+++ backend/ecommerce/src/main/java/com/example/config/JWTConstant.java
@@ -0,0 +1,7 @@
+package com.example.config;
+
+public class JWTConstant {
+	public static final String SECRET_KEY = "buhdshfagzgftyafklmweklrucxnebiuysbewgrwyughjwerlkjoisdejeje";
+	public static final String JWT_HEADER = "Authorization";
+
+}
